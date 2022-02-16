c28e77916bb651bebddcd20a1632dddc24277d61
Dominick Simone <domsimone123@gmail.com>
2022-02-16 15:38:48 -0500

    adding mobile responsiveness

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,7 +26,7 @@ const Projects = () => {
             </div>
             <div className="projectDiv">
                 <div class="projectLinks">
-                    <h3 className="projectTitle projectLinks"><a href="https://github.com/Dominick-Simone/NFL_Bets" className="linkStyles" target="_blank">Github</a></h3>
+                    <h3 className="projectTitle"><a href="https://github.com/Dominick-Simone/NFL_Bets" className="linkStyles" target="_blank">Github</a></h3>
                     <h1 className="projectTitle">NFL Bets</h1>
                     <h3 className="projectTitle"><a href="https://vast-eyrie-51403.herokuapp.com/" className="linkStyles" target="_blank">Deployed</a></h3>
                 </div>
@@ -38,14 +38,14 @@ const Projects = () => {
                     <img src={nflBetsIMG} className="projectImage" alt="NFL Bets" />
                 </a>
                 <div className='projectMobile'>
-                    <h3 className="projectTitle projectLinks"><a href="https://github.com/Dominick-Simone/NFL_Bets" className="linkStyles" target="_blank">Github</a></h3>
+                    <h3 className="projectTitle"><a href="https://github.com/Dominick-Simone/NFL_Bets" className="linkStyles" target="_blank">Github</a></h3>
                     <h3 className="projectTitle"><a href="https://vast-eyrie-51403.herokuapp.com/" className="linkStyles" target="_blank">Deployed</a></h3>
                 </div>
                 <h3 className="projectText">This application uses an NFL API to get information about the NFL game schedule to allow users to choose to bet on whether a team will win or lose in the specified game. The user has to make an account, and then they can choose a favorite team and start betting. This application was created in NodeJS, it uses MySQL as the database, and ExpressJS for the server.</h3>
             </div>
             <div className="projectDiv">
                 <div class="projectLinks">
-                    <h3 className="projectTitle projectLinks"><a href="https://github.com/Dominick-Simone/Weather-App" className="linkStyles" target="_blank">Github</a></h3>
+                    <h3 className="projectTitle"><a href="https://github.com/Dominick-Simone/Weather-App" className="linkStyles" target="_blank">Github</a></h3>
                     <h1 className="projectTitle">Weather App</h1>
                     <h3 className="projectTitle"><a href="https://dominick-simone.github.io/Weather-App/" className="linkStyles" target="_blank">Deployed</a></h3>
                 </div>
@@ -56,14 +56,14 @@ const Projects = () => {
                     <img src={weatherAppIMG} className="projectImage" alt="NFL Bets" />
                 </a>
                 <div className='projectMobile'>
-                    <h3 className="projectTitle projectLinks"><a href="https://github.com/Dominick-Simone/NFL_Bets" className="linkStyles" target="_blank">Github</a></h3>
+                    <h3 className="projectTitle"><a href="https://github.com/Dominick-Simone/NFL_Bets" className="linkStyles" target="_blank">Github</a></h3>
                     <h3 className="projectTitle"><a href="https://vast-eyrie-51403.herokuapp.com/" className="linkStyles" target="_blank">Deployed</a></h3>
                 </div>
                 <h3 className="projectText">This application uses a Weather API to generate the cities weather details, and five day forecast based on the city that the user searches.</h3>
             </div>
             <div className="projectDiv">
                 <div class="projectLinks">
-                    <h3 className="projectTitle projectLinks"><a href="https://github.com/Dominick-Simone/Note-Taker" className="linkStyles" target="_blank">Github</a></h3>
+                    <h3 className="projectTitle"><a href="https://github.com/Dominick-Simone/Note-Taker" className="linkStyles" target="_blank">Github</a></h3>
                     <h1 className="projectTitle">Note Taker</h1>
                     <h3 className="projectTitle"><a href="https://thawing-coast-14961.herokuapp.com/" className="linkStyles" target="_blank">Deployed</a></h3>
                 </div>
@@ -74,7 +74,7 @@ const Projects = () => {
                     <img src={noteTakerIMG} className="projectImage" alt="NFL Bets" />
                 </a>
                 <div className='projectMobile'>
-                    <h3 className="projectTitle projectLinks"><a href="https://github.com/Dominick-Simone/Note-Taker" className="linkStyles" target="_blank">Github</a></h3>
+                    <h3 className="projectTitle"><a href="https://github.com/Dominick-Simone/Note-Taker" className="linkStyles" target="_blank">Github</a></h3>
                     <h3 className="projectTitle"><a href="https://thawing-coast-14961.herokuapp.com/" className="linkStyles" target="_blank">Deployed</a></h3>
                 </div>
                 <h3 className="projectText">This application allows users to create notes that will be saved to a json file instead of a database. It also allows user to delete previous notes and will update the json file with the new list of notes after each note is deleted.</h3>
