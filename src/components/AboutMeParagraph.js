0b0d8c05efe55a6b24573306b89a44d778562b76
Dominick Simone <domsimone123@gmail.com>
2022-09-17 14:05:17 -0400

    Updating Bio

--- src/components/AboutMeParagraph.js
+++ src/components/AboutMeParagraph.js
@@ -4,7 +4,7 @@ import "../styles/aboutmeparagraph.css"
 import AboutAnimation from './AboutAnimation'
 const AboutMeParagraph = () => {
 
-    const text = "Hello, my name is Dominick Simone. I am a 19 year old Full Stack Developer from Lewisburg, PA. I graduated from the full stack coding bootcamp at the University of Pennsylvania powered by Trilogy on November 13th, 2021. I am currently looking for a full time development job!"
+    const text = "Hello, my name is Dominick Simone. I am a 20 year old Full Stack Developer from Lewisburg, PA. I graduated from the Full Stack Coding Bootcamp at the University of Pennsylvania on November 13th, 2021. I am currently working as a Senior Learning Assistant for Full Stack Development Bootcamps at Trilogy Education Services. I am also looking for a full time software development positon!"
 
 
     return (
